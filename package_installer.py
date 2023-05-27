d423bf958c24b249821b21000123d4c289ade980
gitNinja21 <133601121+gitNinja21@users.noreply.github.com>
2023-05-27 18:52:40 +0530

    Add files via upload

---
+++ package_installer.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 27 17:54:41 2023
+
+@author: HP
+"""
+import subprocess
+
+# List of packages to install or upgrade
+required_packages = ['numpy', 'pandas', 'statsmodels','yfinance']
+
+# Iterate over the packages and install or upgrade them
+for package in required_packages:
+    subprocess.run(['pip', 'install', '--upgrade', package], check=True)
