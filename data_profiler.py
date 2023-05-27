4fb80df7c1092344221cc61ba5b40a1d2bf4bc41
gitNinja21 <133601121+gitNinja21@users.noreply.github.com>
2023-05-27 18:53:09 +0530

    Add files via upload

---
+++ data_profiler.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 15 02:30:00 2023
+
+@author: arghya.ghosh
+"""
+
+import numpy as np
+from stock_data_loader import nifty_data_loader
+from statsmodels.tsa.ar_model import ar_select_order
+
+
+
+
+class data_profiling(nifty_data_loader):
+    def __init__(self,**kwargs):
+        self.data=super().__init__(**kwargs)
+        self.lag=kwargs['lag']
+       
+        
+        
+        
+    def _valuation_flag(self):
+        '''
+        
+        Check whether the stock is overvalued or undervalued
+        Returns
+        -------
+        string
+
+        '''
+        
+        raise NotImplementedError("Not Implemented Yet")
+        
+    def _buy_sell_indicator(self):
+        '''
+        Checks whether we should buy the stock or sell the stock
+
+        Returns
+        -------
+        dictionary
+
+        '''
+        raise NotImplementedError("Not Implemented Yet")
+        
+        
+        
+    def _statement_ratios(self):
+        '''
+        Fetches statement ratios
+
+        Returns
+        -------
+        dict
+
+        '''
+        
+        raise NotImplementedError("Not Implemented Yet")
+        
+    def _lag_correlation_variables(self):
+        '''
+        Finds which lag orders are significant
+
+        Returns
+        -------
+        dict
+
+        '''
+        log_return= np.log(self.data['Close'] /self.data['Close'].shift(1))
+        search = ar_select_order(endog=log_return, maxlag=self.lag)
+        return search.ar_lags
+
+
+        
+        
+        
+    
